9e4706dcca72d686125216989318303ecac1ea66
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-06-04 14:36:12 +0530

    Added 3 solution

--- contest13/2.cpp
+++ contest13/2.cpp
@@ -18,12 +18,26 @@ void solve()
     ll n, f, k;
     cin >> n >> f >> k;
     vector<ll> a(n);
-    for (ll i = 0; i < n; i++) cin >> a[i];
+    for (ll i = 0; i < n; i++)
+        cin >> a[i];
+    if (k == n){
+        cout << "YES\n";
+        return ;
+    }
     ll fav = a[f - 1];
     sort(a.begin(), a.end(), greater<ll>());
+
+    /*
     ll cnt_fav = count(a.begin(), a.begin() + k, fav);
     ll total = count(a.begin(), a.end(), fav);
     if (cnt_fav == 0) cout << "NO\n";
     else if (total == cnt_fav) cout << "YES\n";
     else cout << "MAYBE\n";
+    */
+    if (a[k - 1] <= fav and a[k] >= fav)
+        cout << "MAYBE\n";
+    else if (fav > a[k])
+        cout << "YES\n";
+    else
+        cout << "NO\n";
 }
--- contest13/3.cpp
+++ contest13/3.cpp
@@ -3,119 +3,56 @@ using namespace std;
 
 typedef long long ll;
 
-string getInput() {
-    string s;
-    getline(cin, s);
-    return s;
+void solve();
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+        solve();
 }
 
-int getInputInt() {
-    return stoi(getInput());
-}
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for(auto &x: a) cin >> x;
+    for(auto &x: b) cin >> x;
+    ll m;
+    cin >> m;
+    vector<ll> d(m);
+    for(auto &x: d) cin >> x;
 
-vector<int> split(string s) {
-    vector<int> result;
-    size_t pos = 0;
-    while ((pos = s.find(' ')) != string::npos) {
-        result.push_back(stoi(s.substr(0, pos)));
-        s.erase(0, pos + 1);
-    }
-    result.push_back(stoi(s));
-    return result;
-}
-
-vector<vector<int>> transpose(vector<vector<int>>& mat) {
-    int n = mat.size();
-    vector<vector<int>> t(n, vector<int>(n, 0));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            t[j][i] = mat[i][j];
+    multiset<ll> ms;
+    for(int i = 0; i < n; i++){
+        if(a[i] != b[i]){
+            ms.insert(b[i]); // I need to change the b[i] to make it a[i] using dj where 0 <= j <= m-1;
         }
-    }
-    return t;
-}
-
-void fun() {
-    int n, m;
-    vector<int> array = split(getInput());
-    n = array[0];
-    m = array[1];
-    vector<vector<int>> mat1(n);
-    vector<vector<int>> mat2(n);
-    for (int i = 0; i < n; i++) {
-        mat1[i] = split(getInput());
-    }
-    for (int i = 0; i < n; i++) {
-        mat2[i] = split(getInput());
     }
 
-    if (n == m) {
-        set<set<int>> s_of_set;
-        for (auto v : mat1) {
-            set<int> cur(v.begin(), v.end());
-            s_of_set.insert(cur);
+    // check whether d is able to provide the number to change b[i] to a[i]
+    for(auto &x: d){
+        auto it = ms.find(x);
+        if(it != ms.end()){
+            ms.erase(it); // changed b[i] to a[i]
         }
-        for (auto v : mat2) {
-            set<int> cur(v.begin(), v.end());
-            if (s_of_set.find(cur) == s_of_set.end()) {
-                cout << "NO" << endl;
-                return;
-            }
-        }
-        mat1 = transpose(mat1);
-        mat2 = transpose(mat2);
-        s_of_set.clear();
-        for (auto v : mat1) {
-            set<int> cur(v.begin(), v.end());
-            s_of_set.insert(cur);
-        }
-        for (auto v : mat2) {
-            set<int> cur(v.begin(), v.end());
-            if (s_of_set.find(cur) == s_of_set.end()) {
-                cout << "NO" << endl;
-                return;
-            }
-        }
-        cout << "YES" << endl;
-    } else {
-        set<set<int>> s_of_set;
-        for (auto v : mat1) {
-            set<int> cur(v.begin(), v.end());
-            s_of_set.insert(cur);
-        }
-        for (auto v : mat2) {
-            set<int> cur(v.begin(), v.end());
-            if (s_of_set.find(cur) == s_of_set.end()) {
-                cout << "NO" << endl;
-                return;
-            }
-        }
-        s_of_set.clear();
-        for (int j = 0; j < m; j++) {
-            set<int> cur;
-            for (int i = 0; i < n; i++) {
-                cur.insert(mat1[i][j]);
-            }
-            s_of_set.insert(cur);
-        }
-        for (int j = 0; j < m; j++) {
-            set<int> cur;
-            for (int i = 0; i < n; i++) {
-                cur.insert(mat2[i][j]);
-            }
-            if (s_of_set.find(cur) == s_of_set.end()) {
-                cout << "NO" << endl;
-                return;
-            }
-        }
-        cout << "YES" << endl;
     }
-}
 
-int main() {
-    int t = getInputInt();
-    for (int i = 0; i < t; i++) {
-        fun();
+    if(ms.empty() == false){
+        // if all elements were not changed thena ans if NO
+        cout << "NO\n";
+        return ;
     }
-    return 0;
-}
+
+    ll last = d.back(); // the extra elements will be changed with last value and at d[m-1] last will replace last to make array b equal to a
+    bool flag = false;
+    for(auto &x: b){
+        if(x == last){
+            flag = true;
+        }
+    }
+
+    (flag == true) ? cout << "YES\n" : cout << "NO\n";
+}   
