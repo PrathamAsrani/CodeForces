6e10c09930f9e7ad22abe759bf4b8cfee01a6a32
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-06-14 18:54:52 +0530

    deafault Updated

--- default.cpp
+++ default.cpp
@@ -5,6 +5,7 @@ typedef long long ll;
 
 #define ii ({ll a; cin >> a; a;})
 #define si ({string string_input; cin >> string_input; string_input;})
+#define vll vector<ll>
 
 ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b); }
 ll lcm(ll a, ll b) { return a/gcd(a, b) * b; }
@@ -18,19 +19,16 @@ void solve();
 
 int main()
 {
-    // ios::sync_with_stdio(false);
-    // cin.tie(0);
-    // cout.tie(0);
-    ll t;
-    cin >> t;
-    while (t--)
-        solve();
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = ii;
+    while (t--)solve();
 }
 
 void solve()
 {
-    ll n;
-    cin >> n;
-    vector<ll> v(n);
-    for(auto &num: v) cin >> num;
+    ll n = ii;
+    vll v(n);
+    for(auto &num: v) num = ii;
 }   
